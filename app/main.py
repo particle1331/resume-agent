f9307309b3c967b23993575ca8851ee87c8c8672
particle1331 <54845098+particle1331@users.noreply.github.com>
2025-10-20 02:34:00 +0800

    Remove users paths

--- app/main.py
+++ app/main.py
@@ -111,43 +111,6 @@ def chat(ws):
         raise
 
 
-@app.route("/users")
-def users():
-    """Display users as HTML"""
-    if not db:
-        return "<h1>Database not connected!</h1>"
-
-    users = db.get_all_users()
-
-    html = "<h1>Users in Database</h1>"
-    html += "<table border='1' cellpadding='10'>"
-    html += "<tr><th>ID</th><th>Name</th><th>Email</th></tr>"
-
-    for user in users:
-        html += f"""
-        <tr>
-            <td>{user['user_id']}</td>
-            <td>{user['role']}</td>
-            <td>{user['email']}</td>
-        </tr>
-        """
-
-    html += "</table>"
-    html += "<br><a href='/'>Back to home</a>"
-
-    return html
-
-
-@app.route("/users/json")
-def users_json():
-    """Display users as JSON"""
-    if not db:
-        return jsonify({"error": "Database not connected"})
-
-    users = db.get_all_users()
-    return jsonify({"users": users})
-
-
 @app.route("/test")
 def test():
     return '<img src="/static/images/gorilla.png">'
